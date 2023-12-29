090e4a110a19a2f791182b9af37f6b6bf5191cc2
Nagendra766 <154978235+Nagendra766@users.noreply.github.com>
2023-12-29 14:13:51 +0530

    Update mywebapp.py

--- mywebapp.py
+++ mywebapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title='cars')
-st.markdown("## types of cars")
+st.markdown("types of cars")
 st.write("- KIA")
 st.image("https://cdn-images.motor.es/image/m/800w.webp/fotos-noticias/2021/12/precio-kia-sportage-2022-202183808-1640722933_1.jpg")        
             
